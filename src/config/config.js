de1a35491a8eb9fe4e505d9c070ea75a669eb558
unknown <you@example.com>
2019-08-16 10:25:14 -0300

    subindo o banco

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,7 @@
 class Config {
   constructor() {
-    this.mongooseUrl = `mongodb://teste:mudar123@ds259347.mlab.com:59347/fintech-teste`;
+    // this.mongooseUrl = `mongodb://teste:mudar123@ds259347.mlab.com:59347/fintech-teste`;
+    this.mongooseUrl = `mongodb://user:user123@ds163757.mlab.com:63757/dbfintech02`;
     this.origem = "fathomless-beach-11782";
     this.codigoIdentificacaoFintech = 503;
   }
